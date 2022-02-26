fcf3bcd60e991224ba95ed30ab02a2fc7f21331d
amyfranses <amyfranses@gmail.com>
2022-02-26 04:43:42 +0000

    add printPassTimes function, add error handling

--- index2.js
+++ index2.js
@@ -1,10 +1,25 @@
-const {
-  fetchMyIP,
-  fetchCoordsByIP,
-  fetchISSFlyOverTimes,
-} = require("./iss_promised.js");
+const { nextISSTimesForMyLocation } = require("./iss_promised.js");
 
-fetchMyIP()
-  .then(fetchCoordsByIP)
-  .then(fetchISSFlyOverTimes)
-  .then((body) => console.log(JSON.parse(body).response));
+// fetchMyIP()
+//   .then(fetchCoordsByIP)
+//   .then(fetchISSFlyOverTimes)
+//   .then((body) => console.log(JSON.parse(body).response));
+
+// const { nextISSTimesForMyLocation } = require("./iss_promised");
+
+const printPassTimes = function (passTimes) {
+  for (const pass of passTimes) {
+    const datetime = new Date(0);
+    datetime.setUTCSeconds(pass.risetime);
+    const duration = pass.duration;
+    console.log(`Next pass at ${datetime} for ${duration} seconds!`);
+  }
+};
+
+nextISSTimesForMyLocation()
+  .then((passTimes) => {
+    printPassTimes(passTimes);
+  })
+  .catch((error) => {
+    console.log("It didn't work: ", error.message);
+  });
